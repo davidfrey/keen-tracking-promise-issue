56867cb14e2829a99de9a3cf7cddb373d708f653
David Frey <david_frey@mentor.com>
2017-01-19 15:54:03 -0800

    Add suggested force exit

--- __tests__/index.js
+++ __tests__/index.js
@@ -5,3 +5,8 @@ describe('track()', () => {
     track().then(r => expect(r).toBe(true));
   });
 });
+
+// Suggested line from Keen
+process.on('uncaughtException', (err) => {
+ require('fs').writeSync(1, `Caught exception: ${err}, ${err.stack}`);
+});
